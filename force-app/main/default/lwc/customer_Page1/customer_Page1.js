920519be591a0151e24762f6b3d4cb35f49f8910
unknown <anujavaidya2000@gmail.com>
2023-12-09 22:15:38 +0530

    Third

--- force-app/main/default/lwc/customer_Page1/customer_Page1.js
+++ force-app/main/default/lwc/customer_Page1/customer_Page1.js
@@ -1,5 +1,6 @@
 import { LightningElement, track, wire, api} from 'lwc';
 import IMAGES from "@salesforce/resourceUrl/static_images";
+import getStarters from "@salesforce/apex/Customer_Page.getStarters";
 export default class TabsetBasic extends LightningElement {
     section = '';
     handleSectionToggle(event) {
@@ -8,6 +9,16 @@ export default class TabsetBasic extends LightningElement {
     handleClick(event){
        this.template.querySelector('lightning-tabset').activeTabValue = event.target.value;
    }
-    companyLogo = IMAGES + '/Starters/paneer-tikka-skewers.jpg';
+   starters = '';
+   @wire(getStarters)
+    wiredStarters({ error, data }) {
+    if (data) {
+        this.starters = IMAGES + '/Starters/'+data[0].ImageName__c;
+    } else if (error) {
+        this.error = error;
+    }
+
+}
+    //companyLogo = IMAGES + '/Starters/paneer-tikka-skewers.jpg';
     
 }
