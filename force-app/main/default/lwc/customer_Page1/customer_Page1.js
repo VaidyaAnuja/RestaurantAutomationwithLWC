6ad0e4f90a8ee897f409f5e219504dac014f1965
unknown <anujavaidya2000@gmail.com>
2023-12-17 19:55:14 +0530

    Fourth

--- force-app/main/default/lwc/customer_Page1/customer_Page1.js
+++ force-app/main/default/lwc/customer_Page1/customer_Page1.js
@@ -13,12 +13,23 @@ export default class TabsetBasic extends LightningElement {
    @wire(getStarters)
     wiredStarters({ error, data }) {
     if (data) {
-        this.starters = IMAGES + '/Starters/'+data[0].ImageName__c;
+        for(var i=0; i<data.length; i++){
+        let img = document.createElement("img");
+        img.src= IMAGES + '/Starters/'+data[i].ImageName__c;
+        img.style.width = "150px";
+        img.style.height = "150px";
+        img.style.marginRight = "20px";
+        this.refs.myDiv.appendChild(img);
+    }for(var i=0; i<data.length; i++){
+        let lab = document.createElement("h4");
+        lab.textContent = data[i].MasterLabel;
+        this.refs.myDiv1.appendChild(lab);
+
+        }
     } else if (error) {
         this.error = error;
     }
 
 }
-    //companyLogo = IMAGES + '/Starters/paneer-tikka-skewers.jpg';
     
 }
